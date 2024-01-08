962942bb06569aa4dc164050b604f1bdbbf9b3ef
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-08 17:12:40 +0900

    price error

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from .views import store_list, store_detail, menu_price, rate_store, get_average
 urlpatterns = [
     path('stores/', store_list, name='store_list'),
     path('stores/<int:store_id>/', store_detail, name='store_detail'),
-    path('stores/<int:store_id>/price/', menu_price, name='menu_price'),
+    path('stores/<int:store_id>/price/<int:menu_id>/', menu_price, name='menu_price'),  # menu_id 추가
     path('star/<int:store_id>/', rate_store, name='rate_store'),
     path('star/<int:store_id>/', get_average_rating, name='get_average_rating'),
 ]
