78e0fa7ed023a9e3884a9228ea764c72c396aa54
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-08 17:06:34 +0900

    store price

--- store/urls.py
+++ store/urls.py
@@ -5,8 +5,7 @@ from .views import store_list, store_detail, menu_price, rate_store, get_average
 urlpatterns = [
     path('stores/', store_list, name='store_list'),
     path('stores/<int:store_id>/', store_detail, name='store_detail'),
-    path('stores/<int:store_id>/<int:menu_id>/', store_detail, name='store_detail'),
-    path('stores/<int:store_id>/<int:menu_id>/price/', menu_price, name='menu_price'),
+    path('stores/<int:store_id>/price/', menu_price, name='menu_price'),
     path('star/<int:store_id>/', rate_store, name='rate_store'),
     path('star/<int:store_id>/', get_average_rating, name='get_average_rating'),
 ]
