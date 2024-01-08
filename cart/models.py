062cad5368b856528acac4f134708fa347188dc8
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-08 17:23:30 +0900

    장바구니 가격 int 형식으로 반환

--- cart/models.py
+++ cart/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class CartItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField()
 
 
 
