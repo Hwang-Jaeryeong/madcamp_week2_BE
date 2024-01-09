042134d41c345ecf782dc87ff9c08b6e1ef93b51
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-09 15:29:45 +0900

    [add] card_item에 store name 추가

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,8 +1,17 @@
-# serializers.py
+# cart/serializers.py
 from rest_framework import serializers
 from .models import CartItem
+from store.serializers import StoreSerializer  # StoreSerializer를 import
 
 class CartItemSerializer(serializers.ModelSerializer):
+    store = StoreSerializer()  # StoreSerializer를 사용하여 store 필드를 시리얼화
+
     class Meta:
         model = CartItem
-        fields = ['id', 'product_name', 'price']
+        fields = ['id', 'product_name', 'price', 'store']  # store 필드를 포함
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['store_name'] = representation['store']['name']
+        return representation
+
