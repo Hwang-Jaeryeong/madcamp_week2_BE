d3024dbdd865a85068890db98fcf379ab2891266
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-09 23:29:39 +0900

    ㅈㅂ

--- star/models.py
+++ star/models.py
@@ -1,12 +1,12 @@
 # star/models.py
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Store(models.Model):
     name = models.CharField(max_length=255)
 
 class Star(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)  # 수정
     rating = models.IntegerField()
