bd4f820efa2bd22dd415656894824eff5930f1b8
Hwang-Jaeryeong <jryeong67@naver.com>
2024-01-09 17:35:02 +0900

    [fix] order

--- cart/models.py
+++ cart/models.py
@@ -13,7 +13,7 @@ class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255)
     store_name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .models import CartItem
-from .serializers import CartItemSerializer
+from .serializers import CartItemSerializer, OrderSerializer
+from .models import Order
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
